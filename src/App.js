10180907c4e24db2fd59e40a11499c952f630fc3
Edson Sales <edsonsales99@gmail.com>
2022-12-18 21:43:38 -0400

    Listando todas as operações

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import './App.css';
+import {useEffect, useState} from "react"; //2.9k (gzipped: 1.3k)
+
+
+type Repository = {
+ // dataTransferencia: Date;
+  valor: Number[];
+ // tipo: String;
+  operadorTransacao: String[];
+//  contaId: Number;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edson Sales
-        </p>
-     
-      </header>
 
-    </div>
-  );
+    const [repositories, setRepositories] = useState<Repository[]>([])
+
+    useEffect(() => {
+      fetch('http://localhost:8080/operacao')
+        .then(response => response.json())
+        .then(data => {
+          setRepositories(data);
+        })
+    }, [])
+
+    return(
+      <ul>
+        {repositories.map(repo => {
+          return(
+            <li key={repo.tipo}>
+              <strong>{repo.tipo}</strong>
+              <p>{repo.operadorTransacao}</p>
+            </li>
+          )
+        })}
+      </ul>
+    )
 }
 
 export default App;
